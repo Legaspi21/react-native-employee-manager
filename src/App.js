9a1f988e22a2f5385f32a2fe2502adb6398998ea
Aidan Legaspi <aidanlegaspi1@gmail.com>
2017-02-24 13:18:26 -0800

    Import LoginForm component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+// import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import firebase from 'firebase';
 import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
 	componentWillMount() {
@@ -19,11 +20,7 @@ class App extends Component {
 	render() {
 		return (
 			<Provider store={createStore(reducers)}>
-				<View>
-					<Text>
-						Hello!
-					</Text>
-				</View>
+				<LoginForm />
 			</Provider>
 		);
 	}
