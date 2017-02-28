3607a119240dd635e3839b5fe3e77cb5eb877e71
Aidan Legaspi <aidanlegaspi1@gmail.com>
2017-02-28 11:30:05 -0800

    Add passwordChanged action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,21 @@
-import { EMAIL_CHANGED } from './types';
+import firebase from 'firebase';
+import { EMAIL_CHANGED, PASSWORD_CHANGED } from './types';
 
 export const emailChanged = (text) => {
 	return {
 		type: EMAIL_CHANGED,
 		payload: text
 	};
+};
+
+export const passwordChanged = (text) => {
+	return {
+		type: PASSWORD_CHANGED,
+		payload: text
+	};
+};
+
+export const loginUser = ({ email, password }) => {
+	firebase.auth.signInWithEmailAndPassword(email, password)
+		.then(user => console.log(user));
 };
