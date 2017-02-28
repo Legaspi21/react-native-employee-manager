2e6b34e756451c447dfd3ddb3435325b1bf3a040
Aidan Legaspi <aidanlegaspi1@gmail.com>
2017-02-27 17:25:44 -0800

    Implements mapStateToProps; Passes email state to component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -16,7 +16,7 @@ class LoginForm extends Component {
 						label="email"
 						placeholder="user@gmail.com"
 						onChangeText={this.onEmailChange.bind(this)}
-
+						value={this.props.email}
 					/>
 				</CardSection>
 
@@ -39,4 +39,10 @@ class LoginForm extends Component {
 	}
 }
 
-export default connect(null, emailChanged)(LoginForm);
+const mapStateToProps = state => {
+	return {
+		email: state.auth.email
+	};
+};
+
+export default connect(mapStateToProps, { emailChanged })(LoginForm);
