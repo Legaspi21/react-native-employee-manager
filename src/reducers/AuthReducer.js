a198fe7e85fc272905bc4888002cc1e4daf81006
Aidan Legaspi <aidanlegaspi1@gmail.com>
2017-03-02 21:46:19 -0800

    Error handling; Returns new state with error to display and clears password field

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,13 +1,13 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from '../actions/types';
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL } from '../actions/types';
 
 const INITIAL_STATE = { 
 	email: '',
 	password: '', 
-	user: null 
+	user: null,
+	error: null 
 };
 
 export default (state = INITIAL_STATE, action) => {
-	console.log(action);
 	
 	switch (action.type) {
 		case EMAIL_CHANGED: 
@@ -19,6 +19,8 @@ export default (state = INITIAL_STATE, action) => {
 		case LOGIN_USER_SUCCESS:
 			return { ...state, user: action.payload };
 
+		case LOGIN_USER_FAIL:
+			return { ...state, error: 'Authentication Failed.', password: '' };
 
 		default:
 			return state;
