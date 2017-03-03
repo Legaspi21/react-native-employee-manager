f8701a85fcd6cba6b47f00b6625c2d60b11a8dd2
Aidan Legaspi <aidanlegaspi1@gmail.com>
2017-03-03 01:31:10 -0800

    Implement Picker for shift and overwrite styling for coresponding CardSection component

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Picker, Text } from 'react-native';
+import { employeeUpdate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
+
 
 class EmployeeCreate extends Component {
 	render() {
@@ -9,6 +13,8 @@ class EmployeeCreate extends Component {
 					<Input 
 						label="Name"
 						placeholder="Jane"
+						value={this.props.name}
+						onChangeText={value => this.props.employeeUpdate({ prop: 'name', value })}
 					/>
 				</CardSection>
 
@@ -16,11 +22,25 @@ class EmployeeCreate extends Component {
 					<Input 
 						label="Phone"
 						placeholder="555-555-5555"
+						value={this.props.phone}
+						onChangeText={value => this.props.employeeUpdate({ prop: 'phone', value })}
 					/>
 				</CardSection>
 
-				<CardSection>
-					
+				<CardSection style={{ flexDirection: 'column' }}>
+					<Text style={styles.pickerTextStyle}>Shift</Text>
+					<Picker
+						selectedValue={this.props.shift}
+						onValueChange={value => this.props.employeeUpdate({ prop: 'shift', value })}
+					>
+						<Picker.Item label="Monday" value="Monday" />
+						<Picker.Item label="Tuesday" value="Tuesday" />
+						<Picker.Item label="Wednesday" value="Wednesday" />
+						<Picker.Item label="Thursday" value="Thursday" />
+						<Picker.Item label="Friday" value="Friday" />
+						<Picker.Item label="Saturday" value="Saturday" />
+						<Picker.Item label="Sunday" value="Sunday" />
+					</Picker>
 				</CardSection>
 
 				<CardSection>
@@ -33,4 +53,17 @@ class EmployeeCreate extends Component {
 	}
 }
 
-export default EmployeeCreate;
+const styles = {
+	pickerTextStyle: {
+		fontSize: 18,
+		paddingLeft: 20
+	}
+};
+
+const mapStateToProps = (state) => {
+	const { name, phone, shift } = state.employeeForm;
+
+	return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
